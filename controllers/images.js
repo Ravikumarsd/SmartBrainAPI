bd79751f4477f4e84a90d1aef3050bfd6630a319
Ravikumar Daivajna <ravi@Apples-MacBook-Pro.local>
2018-11-03 16:19:53 +0530

    Handeled code and security review

---
+++ controllers/images.js
@@ -0,0 +1,31 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+    apiKey: '3e507f749cfa4015afa4854812165b38'
+   });
+
+const handleAPICall = (req,res) => {
+  app.models
+            .predict(Clarifai.FACE_DETECT_MODEL,req.body.input)
+            .then(data => {
+                res.json(data);
+            })
+           .catch(err => res.status(400).json("Unable to communicate with clarifai API")) 
+}
+
+const handleImages = (req, res, db) => {
+    const { id } = req.body;
+    db('users').where('id', '=', id)
+        .increment('entries', 1)
+        .returning('entries')
+        .then(entries => {
+            console.log(entries[0]);
+            res.json(entries[0]);
+        })
+        .catch(err => res.status(400).json("Unable to get entries"))
+}
+
+module.exports = {
+    handleImages,
+    handleAPICall
+}
--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const bcrypt = require('bcrypt-nodejs');
-const cors = require('cors');
-const knex = require('knex');
-
+const express = require('express')
+const bodyParser = require('body-parser')
+const bcrypt = require('bcrypt-nodejs')
+const cors = require('cors')
+const knex = require('knex')
+const register = require('./controllers/register')
+const signin = require('./controllers/signin')
+const profile = require('./controllers/profile')
+const images = require('./controllers/images')
 const db = knex({
     client: 'pg',
     connection: {
@@ -16,85 +19,21 @@ const db = knex({
 
 //middle-ware
 const app = express();
-app.use(bodyParser.json());
-app.use(cors());
+app.use(bodyParser.json())
+app.use(cors())
 
 
-app.get('/', (req, res) => {
-    res.json(database.users)
-})
+app.get('/', (req, res) => res.json(database.users))
 
-app.post('/signin', (req, res) => {
-    db.select('email','hash').from('login')
-       .where('email', '=', req.body.email)
-       .then(data => {
-           const isValid = bcrypt.compareSync(req.body.password,data[0].hash);
-           if(isValid) {
-               return db.select('*').from('users')
-                      .where('email', '=', req.body.email)
-                      .then(user => {
-                          res.json(user[0])
-                      })
-                      .catch(err => res.status(400).json('unable to get user'))
-           } else {
-               res.status(400).json('wrong credentials')
-           }
-       })
-       .catch(err => res.status(400).json('wrong credentials'))  
-})
+app.post('/signin', signin.handleSignin(db, bcrypt))
 
-app.post('/register', (req, res) => {
-    const { email, name, password } = req.body;
-    const hash = bcrypt.hashSync(password);
-    db.transaction(trx => {
-        trx.insert({
-            hash: hash,
-            email: email
-        })
-            .into('login')
-            .returning('email')
-            .then(loginEmail => {
-                return trx('users')
-                    .returning('*')
-                    .insert({
-                        name: name,
-                        email: loginEmail[0],
-                        joined: new Date()
-                    })
-                    .then(user => {
-                        res.json(user[0])
-                    })
-            })
-            .then(trx.commit)
-            .catch(trx.rollback)
-    })
-        .catch(err => res.status(400).json('Error in registration'))
-})
+app.post('/register', (req,res) => register.handleRegister(req, res, db, bcrypt))
 
-app.get('/profile/:id', (req, res) => {
-    const { id } = req.params;
-    db.select('*').from('users').where({ id })
-        .then(user => {
-            if (user.length) {
-                res.json(user[0]);
-            } else {
-                res.status(404).json("Not found");
-            }
-        })
-        .catch(err => res.status(400).json("Error getting users"));
-})
+app.get('/profile/:id', (req,res) => profile.handleProfile(req,res,db))
 
-app.put('/images', (req, res) => {
-    const { id } = req.body;
-    db('users').where('id', '=', id)
-        .increment('entries', 1)
-        .returning('entries')
-        .then(entries => {
-            console.log(entries[0]);
-            res.json(entries[0]);
-        })
-        .catch(err => res.status(400).json("Unable to get entries"))
-})
+app.put('/images', (req,res) => images.handleImages(req,res,db))
+
+app.post('/imagesurl', (req,res) => images.handleAPICall(req,res))
 
 app.listen(3000, () => {
     console.log("Server is runnin on port 3000");
