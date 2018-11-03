f99301d939c856b0d776dcdd28bbec20e88be588
Ravikumar Daivajna <ravi@Apples-MacBook-Pro.local>
2018-11-03 19:26:25 +0530

    response added

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use(bodyParser.json())
 app.use(cors())
 
 
-app.get('/', (req, res) => res.json(database.users))
+app.get('/', (req, res) => res.json("its working"))
 
 app.post('/signin', signin.handleSignin(db, bcrypt))
 
